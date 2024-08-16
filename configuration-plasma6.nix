d50f2ce8dfbf61861660286b1e60bcca07918df7
Spacecat <69915180+cs-spacecat@users.noreply.github.com>
2024-08-16 19:38:32 +0200

    cleanup

--- configuration-plasma6.nix
+++ configuration-plasma6.nix
@@ -1,33 +1,54 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [
+  system.stateVersion = "24.05";
+  imports = [
       ./hardware-configuration.nix
       (import "${builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz"}/nixos")
     ];
 
-  # enable auto updates
-  system.autoUpgrade.enable = true;
 
-  # Prevent the new user dialog in zsh
-  system.userActivationScripts.zshrc = "touch .zshrc";
+  # ------ test place -----------
 
   # doesent work sadly
   #system.userActivationScripts.script.text = "mpvpaper '*' wp8214759.mp4 -p -o 'no-audio --loop-playlist'";
 
+
+  # ---- end test place ---------
+
+
+  # Prevent the new user dialog in zsh
+  system.userActivationScripts.zshrc = "touch .zshrc";
+  fileSystems."/run/media/spacecat/4tb-hdd".device = "/dev/disk/by-uuid/1c16e4a2-6027-4f80-b880-b37395562a0a";   # Automount HDD
+  security.rtkit.enable = true;
+  systemd.services.atuin.enable = true;
+  system.autoUpgrade.enable = true;  # enable auto updates
+  nixpkgs.config.allowUnfree = true;
+  documentation.nixos.enable = false;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];  # experimental features
+
+
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/nvme0n1";
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/nvme0n1";
+    useOSProber = true;
+  };
 
-  networking.hostName = "Kommandozentrale";
+  users.users.spacecat = {
+    isNormalUser = true;
+    description = "spacecat";
+    extraGroups = [ "networkmanager" "wheel" "dialout" "adbusers" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [ ];
+  };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  networking.firewall.allowedTCPPorts = [];
-  networking.firewall.allowedUDPPorts = [];
+  networking = {
+    hostName = "Kommandozentrale";
+    networkmanager.enable = true;  # Enable networking
+    firewall.allowedTCPPorts = [];
+    firewall.allowedUDPPorts = [];
+  };
 
   # time zone & locale settings
   time.timeZone = "Europe/Berlin";
@@ -44,43 +65,73 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  services = {
+    xserver.enable = true;  # Enable the X11 windowing system.
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+    pcscd.enable = true;  # required for pinentry
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "";
+    # Enable the KDE Plasma Desktop Environment.
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+
+    xserver.xkb.layout = "de";  # German Keymap
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
   };
 
   # Configure console keymap
   console.keyMap = "de";
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  hardware = {
+    pulseaudio.enable = false;
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
+    steam-hardware.enable = true;  # better controller support
   };
-  # experimental features
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  users.users.spacecat = {
-    isNormalUser = true;
-    description = "spacecat";
-    extraGroups = [ "networkmanager" "wheel" "dialout" "adbusers" ];
-    shell = pkgs.zsh;
-    packages = with pkgs; [ ];
+  programs = {
+    steam.remotePlay.openFirewall = true;
+
+    steam.enable = true;
+    adb.enable = true;
+    gnupg.agent.enable = true;
+    zsh.enable = true;
+    zsh.ohMyZsh.enable = true;
+
+    gnupg.agent = {
+      pinentryPackage = pkgs.pinentry-qt;
+      settings = {
+        max-cache-ttl = 0;
+        default-cache-ttl = 0;
+      };
+    };
+
+    zsh = {
+      enableCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+      enableBashCompletion = true;
+      shellAliases = {
+        rebuild = "sudo nixos-rebuild switch";
+        clean = "nix-store --gc";
+        configure = "kate /etc/nixos/configuration.nix";
+      };
+      ohMyZsh = {
+        theme = "gnzh";
+        plugins = [
+          "git"
+          #"history"
+        ];
+      };
+      promptInit = 'eval "$(atuin init zsh)"';
+    };
   };
+
   home-manager.users.spacecat = {
     home.stateVersion = "24.05";
     programs.git = {
@@ -89,13 +140,10 @@
       userName = "cs-spacecat";
     };
   };
-  # Enable automatic login for the user.
-  # services.displayManager.autoLogin.enable = true;
-  # services.displayManager.autoLogin.user = "spacecat";
 
-  nixpkgs.config.allowUnfree = true;
-  services.xserver.excludePackages = with pkgs; [ xterm ];
-  documentation.nixos.enable = false;
+  services.xserver.excludePackages = with pkgs; [
+    xterm
+  ];
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     elisa
     khelpcenter
@@ -103,84 +151,44 @@
     okular
     print-manager
   ];
-
   environment.systemPackages = with pkgs; [
-    # main
-    home-manager
-    python3
+    google-chrome
+    discord
+    steam
+    heroic
+    (opera.override { proprietaryCodecs = true; })  # video steaming support for opera
+
+    # cli
     usbutils
     fzf
     wget
-    google-chrome
-    pinentry-qt
-    gnupg
     unzip
     pstree
     atuin
-    vnstat
-    mpvpaper
-    (opera.override { proprietaryCodecs = true; }) # video steaming support
+    vnstat  # network stat tracker
     ffmpeg-full
-    discord
-    steam
+    #mpvpaper
 
-    #dictionaries
-    aspellDicts.de
-    aspellDicts.en
-    aspellDicts.en-science
-    aspellDicts.en-computers
+    # pgp
+    pinentry-qt
+    gnupg
 
-    # ux
+    # ui
     simp1e-cursors
     utterly-round-plasma-style
 
     # python libs
+    python3
     updog
+
+    # NixOS
+    home-manager
+
+    # dictionaries
+    aspellDicts.de
+    aspellDicts.en
+    aspellDicts.en-science
+    aspellDicts.en-computers
     ];
-
-  # Automount HDD
-  fileSystems."/run/media/spacecat" = {
-    device = "/dev/disk/by-uuid/1c16e4a2-6027-4f80-b880-b37395562a0a";
-  };
-
-  hardware.steam-hardware.enable = true;
-  programs.steam.enable = true;
-  programs.steam.remotePlay.openFirewall = true;
-
-  programs.adb.enable = true;
-  services.pcscd.enable = true;  # required for pinentry
-  programs.gnupg.agent = {
-    enable = true;
-    pinentryPackage = pkgs.pinentry-qt;
-    settings = {
-      max-cache-ttl = 0;
-      default-cache-ttl = 0;
-    };
-  };
-  systemd.services.atuin = {
-    enable = true;
-  };
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    enableBashCompletion = true;
-    shellAliases = {
-      rebuild = "sudo nixos-rebuild switch";
-      clean = "nix-store --gc";
-    };
-    ohMyZsh = {
-      enable = true;
-      theme = "gnzh";
-      plugins = [
-        "git"
-        #"history"
-      ];
-    };
-    promptInit = ''
-        eval "$(atuin init zsh)"
-      '';
-  };
-  system.stateVersion = "24.05";
 }
+
