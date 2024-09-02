c819f7c0ab7aad9002141d78a9f8245e94e3c187
Spacecat <69915180+cs-spacecat@users.noreply.github.com>
2024-09-02 21:37:26 +0200

    nix complaining that variable is not renamed
    fix

--- cups.nix
+++ cups.nix
@@ -18,7 +18,8 @@
 
 	services.avahi = {  # enable printer discovery idk (this just works, dont ask)
 		enable = true;
-		nssmdns = true;
+		nssmdns4 = true;
+		nssmdns6 = true;
 		openFirewall = true;
 		publish = {
 			enable = true;
