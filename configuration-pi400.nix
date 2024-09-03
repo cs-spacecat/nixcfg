52e4d3552ea68b2a0f63c85e0b649a18ea1ca1ee
Spacecat <69915180+cs-spacecat@users.noreply.github.com>
2024-09-03 22:33:32 +0200

    daily commit; added cups to main file, added WebDAV (very dirty but works!), grouped stuff

--- configuration-pi400.nix
+++ configuration-pi400.nix
@@ -9,27 +9,29 @@ let
 
 in {
   imports = [
-    ./cups.nix
     "${fetchTarball "https://github.com/NixOS/nixos-hardware/tarball/master"}/raspberry-pi/4"
   ];
+  hardware.enableRedistributableFirmware = true;
+  system.stateVersion = "24.05";
 
-  boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
-    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
-    loader = {
-      grub.enable = false;
-      generic-extlinux-compatible.enable = true;
-      #grub.device = "/dev/disk/by-label/NIXOS_SD";
-    };
-  };
 
+  # --- Networking ---
   networking = {
     hostName = hostname;
     networkmanager.enable = true;  # Enable networking
-    firewall.allowedTCPPorts = [ ];
-    firewall.allowedUDPPorts = [ ];
+    firewall.allowedTCPPorts = [
+      22  # ssh
+      631  # cups open port
+      4918  # HTTPS WebDAV server
+    ];
+    firewall.allowedUDPPorts = [
+      22  # ssh
+      631  # cups open port
+      4918  # HTTPS WebDAV server
+    ];
   };
 
+  # ---- docker ----
   #virtualisation.docker = {
   #  enable = true;
   #  rootless = {
@@ -40,34 +42,88 @@ in {
   #    data-root = "/home/${user}/docker";
   #  };
   #};
-  environment.systemPackages = with pkgs; [
-    zsh
-  ];
+
+  # --- WebDAV --- (mainly for KeePass) (dirty but works!)
+  systemd.services.dufs-webdav = {
+    description = "dufs WebDAV Server";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      #ExecStart = "${pkgs.dufs}/bin/dufs -p 4918 -a ${user}:${password}@/home/${user}/webDAV:rw --allow-delete --allow-upload --allow-search --allow-archive";
+      WorkingDirectory = "/home/${user}/webDAV";
+      ExecStart = "${pkgs.dufs}/bin/dufs -p 4918 -a ${user}:${password}@/:rw --allow-delete --allow-upload --allow-search --allow-archive";
+      Restart = "always";
+      RestartSec = "10s";
+    };
+  };
+
+  # --- CUPS printing service ---
+  services.printing.enable = true;
+	services.printing.drivers = [
+	#pkgs.gutenprint # — Drivers for many different printers from many different vendors.
+	#pkgs.gutenprintBin # — Additional, binary-only drivers for some printers.
+	pkgs.hplip # — Drivers for HP printers.
+	#pkgs.hplipWithPlugin # — Drivers for HP printers, with the proprietary plugin. Use NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup' to add the printer, regular CUPS UI doesn't seem to work.
+	#pkgs.postscript-lexmark # — Postscript drivers for Lexmark
+	#pkgs.samsung-unified-linux-driver # — Proprietary Samsung Drivers
+	#pkgs.splix # — Drivers for printers supporting SPL (Samsung Printer Language).
+	#pkgs.brlaser # — Drivers for some Brother printers
+	#pkgs.brgenml1lpr #  — Generic drivers for more Brother printers [1]
+	#pkgs.brgenml1cupswrapper  # — Generic drivers for more Brother printers [1]
+	#pkgs.cnijfilter2 # — Drivers for some Canon Pixma devices (Proprietary driver)
+	];
+
+	services.avahi = {  # enable printer discovery (idk this just works, dont ask)
+		enable = true;
+		nssmdns4 = true;
+		nssmdns6 = true;
+		openFirewall = true;
+		publish = {
+			enable = true;
+			userServices = true;
+		};
+	};
+	services.printing = {
+		listenAddresses = [ "*:631" ];
+		allowFrom = [ "all" ];
+		browsing = true;
+		defaultShared = true;
+	};
+	#networking.firewall = {  # open ports to that you can access the printer via IPP
+	#	allowedTCPPorts = [ 631 ];
+	#	allowedUDPPorts = [ 631 ];
+	#};
+	hardware.printers = {
+    ensurePrinters = [{  # add printer declaratively, so that it always appears
+      name = "Laserjet-2200";
+      description = "HP Laserjet 2200D";
+      location = "Wohnzimmer Wandschrank";
+      deviceUri = "usb://HP/LaserJet%202200?serial=00CNHRB43441";
+      model = "HP/hp-laserjet_2200_series.ppd.gz";  #ls /nix/store/*-hplip-*/share/cups/model/HP/ | grep laserjet_2200    "HP/hp-laserjet_2200-ps.ppd.gz"  "HP/hp-laserjet_2200_series.ppd.gz"  "HP/hp-laserjet_2200_series-ps.ppd.gz"
+      ppdOptions = {
+        PageSize = "A4";
+        #Duplex = "DuplexNoTumble";
+      };
+	}];
+  };
+
+  # ---- OpenSSH ----
   services.openssh.settings.PermitRootLogin = "yes";
   services.openssh.enable = true;  # for sftp editing this file
 
-  users = {
-    #mutableUsers = false;
-    users."${user}" = {
-      isNormalUser = true;
-      password = password;
-      extraGroups = [ "wheel"];  # "docker"
-      shell = pkgs.zsh;
-    };
-    users."root".password = password;
-  };
-
+  # ---- Cloudflared ----
   systemd.services.cloudflared_tunnel = {  # for publicly hosting local websites
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     serviceConfig = {
       ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token=${cloudflareToken}";
       Restart = "always";
-      User = "spacecat";
+      User = "${user}";
       #Group = "cloudflared";
     };
   };
 
+  # ---- Zshell ----
   system.userActivationScripts.zshrc = "touch .zshrc";   # Prevent the new user dialog in zsh
   programs = {
     zsh = {
@@ -84,6 +140,36 @@ in {
     promptInit = ''
       '';
     };
+  };
+
+  # --- sys Packages ---
+  environment.systemPackages = with pkgs; [
+    zsh
+    btop
+    cloudflared
+    dufs
+    cups
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+      #grub.device = "/dev/disk/by-label/NIXOS_SD";
+    };
+  };
+
+  users = {
+    #mutableUsers = false;
+    users."${user}" = {
+      isNormalUser = true;
+      password = password;
+      extraGroups = [ "wheel"];  # "docker"
+      shell = pkgs.zsh;
+    };
+    users."root".password = password;
   };
 
   time.timeZone = "Europe/Berlin";
@@ -105,7 +191,4 @@ in {
     fsType = "ext4";
     options = [ "noatime" ];
   };
-
-  hardware.enableRedistributableFirmware = true;
-  system.stateVersion = "24.05";
 }
