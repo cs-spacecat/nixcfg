57026f30db3f4860279d973035b723a8da0ffbcc
Spacecat <69915180+cs-spacecat@users.noreply.github.com>
2024-12-18 22:22:49 +0100

    added vaultwarden, reformatting, removing comments, updating zsh

--- configuration-pi400.nix
+++ configuration-pi400.nix
@@ -1,52 +1,47 @@
 { config, pkgs, lib, ... }:
 
 let
-  user = "";
-  password = "";
-  hostname = "";
+  user            = "";
+  password        = "";
+  hostname        = "";
+  disk            = "";
   cloudflareToken = "";
-  vscodeToken = "";
-  lang = "de_DE.UTF-8";
-  nixVer = "24.05";
+  vscodeToken     = "";
+  lang            = "de_DE.UTF-8";
+  nixVer          = "24.11";
 
 in {
   imports = [
     "${fetchTarball "https://github.com/NixOS/nixos-hardware/tarball/master"}/raspberry-pi/4"
+    #./hardware-configuration.nix
   ];
   hardware.enableRedistributableFirmware = true;
   system.stateVersion = nixVer;
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 	
 
   # --- Networking ---
   networking = {
     hostName = hostname;
+    useDHCP = lib.mkDefault true;
     networkmanager.enable = true;  # Enable networking
     firewall.allowedTCPPorts = [
       22  # ssh
       631  # cups open port
       3401  # vscode Server
       4918  # HTTPS WebDAV server
+      9090  # updog
     ];
     firewall.allowedUDPPorts = [
       22  # ssh
       631  # cups open port
       3401  # vscode Server
       4918  # HTTPS WebDAV server
+      9090  # updog
     ];
   };
 
-  # ---- docker ----
-  #virtualisation.docker = {
-  #  enable = true;
-  #  rootless = {
-  #    enable = true;
-  #    setSocketVariable = true;
-  #  };
-  #  daemon.settings = {
-  #    data-root = "/home/${user}/docker";
-  #  };
-  #};
 
   # --- WebDAV --- (mainly for KeePass) (dirty but works!)
   systemd.services.dufs-webdav = {
@@ -78,51 +73,52 @@ in {
     };
   };
 
-  # --- VSCode server ---
-  #services.openvscode-server = {
-  #  enable = true;
-  #  port = 3401;
-  #  user = user;
-  #  connectionToken = vscodeToken;
-  #  serverDataDir = "/home/${user}/openVSCode";
-  #  #extraArguments = "--update-extensions";
-  #  host = "127.0.0.1";
-  #};
-  
+
+  # --- Vaultwarden ---
+  services.vaultwarden = {
+    enable = true;
+    dbBackend = "sqlite";
+    #backupDir = "/var/backup/vaultwarden";
+    config = {
+      ROCKET_ADRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+    };
+  };
+
 
   # --- CUPS printing service ---
   services.printing.enable = true;
-	services.printing.drivers = [
-	#pkgs.gutenprint # — Drivers for many different printers from many different vendors.
-	#pkgs.gutenprintBin # — Additional, binary-only drivers for some printers.
-	pkgs.hplip # — Drivers for HP printers.
-	#pkgs.hplipWithPlugin # — Drivers for HP printers, with the proprietary plugin. Use NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup' to add the printer, regular CUPS UI doesn't seem to work.
-	#pkgs.postscript-lexmark # — Postscript drivers for Lexmark
-	#pkgs.samsung-unified-linux-driver # — Proprietary Samsung Drivers
-	#pkgs.splix # — Drivers for printers supporting SPL (Samsung Printer Language).
-	#pkgs.brlaser # — Drivers for some Brother printers
-	#pkgs.brgenml1lpr #  — Generic drivers for more Brother printers [1]
-	#pkgs.brgenml1cupswrapper  # — Generic drivers for more Brother printers [1]
-	#pkgs.cnijfilter2 # — Drivers for some Canon Pixma devices (Proprietary driver)
-	];
+    services.printing.drivers = [
+    #pkgs.gutenprint # — Drivers for many different printers from many different vendors.
+    #pkgs.gutenprintBin # — Additional, binary-only drivers for some printers.
+    pkgs.hplip # — Drivers for HP printers.
+    #pkgs.hplipWithPlugin # — Drivers for HP printers, with the proprietary plugin. Use NIXPKGS_ALLOW_UNFREE=1 nix-shell -p hplipWithPlugin --run 'sudo -E hp-setup' to add the printer, regular CUPS UI doesn't seem to work.
+    #pkgs.postscript-lexmark # — Postscript drivers for Lexmark
+    #pkgs.samsung-unified-linux-driver # — Proprietary Samsung Drivers
+    #pkgs.splix # — Drivers for printers supporting SPL (Samsung Printer Language).
+    #pkgs.brlaser # — Drivers for some Brother printers
+    #pkgs.brgenml1lpr #  — Generic drivers for more Brother printers [1]
+    #pkgs.brgenml1cupswrapper  # — Generic drivers for more Brother printers [1]
+    #pkgs.cnijfilter2 # — Drivers for some Canon Pixma devices (Proprietary driver)
+  ];
 
-	services.avahi = {  # enable printer discovery (idk this just works, dont ask)
-		enable = true;
-		nssmdns4 = true;
-		nssmdns6 = true;
-		openFirewall = true;
-		publish = {
-			enable = true;
-			userServices = true;
-		};
-	};
-	services.printing = {
-		listenAddresses = [ "*:631" ];
-		allowFrom = [ "all" ];
-		browsing = true;
-		defaultShared = true;
-	};
-	hardware.printers = {
+  services.avahi = {  # enable printer discovery (idk this just works, dont ask)
+    enable = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+  services.printing = {
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
+  };
+  hardware.printers = {
     ensurePrinters = [{  # add printer declaratively, so that it always appears
       name = "Laserjet-2200";
       description = "HP Laserjet 2200D";
@@ -133,7 +129,7 @@ in {
         PageSize = "A4";
         #Duplex = "DuplexNoTumble";
       };
-	}];
+    }];
   };
 
   # ---- OpenSSH ----
@@ -162,12 +158,42 @@ in {
       syntaxHighlighting.enable = true;
       enableBashCompletion = true;
       shellAliases = {
-        rebuild   = "sudo nixos-rebuild switch --show-trace";
-        clean     = "nix-store --gc";
-        configure = "sudo nano /etc/nixos/configuration.nix && rebuild";
+        rebuild   = "sudo time nixos-rebuild switch --show-trace";
+        clean     = "time nix-store --gc && sudo time nix-collect-garbage -d";
+        configure = "sudo nano /etc/nixos/configuration.nix && time rebuild";
+        o         = "xdg-open . &";
+        open      = "xdg-open . &";
+        sl        = "${pkgs.sl}/bin/sl -Gw";
       };
-    promptInit = ''
+      shellInit = ''
+        ex () {  # ex - archive extractor
+          if [ -f $1 ] ; then
+            case $1 in
+              *.tar.bz2)   tar xjf $1   ;;
+              *.tar.gz)    tar xzf $1   ;;
+              *.tar.xz)    tar xJf $1   ;;
+              *.bz2)       bunzip2 $1   ;;
+              *.rar)       unrar x $1     ;;
+              *.gz)        gunzip $1    ;;
+              *.tar)       tar xf $1    ;;
+              *.tbz2)      tar xjf $1   ;;
+              *.tgz)       tar xzf $1   ;;
+              *.zip)       unzip $1     ;;
+              *.Z)         uncompress $1;;
+              *.7z)        7z x $1      ;;
+              *)           echo "'$1' cannot be extracted via ex()" ;;
+            esac
+          else
+            echo "'$1' is not a valid file"
+          fi
+        }
       '';
+      ohMyZsh = {
+        enable = true;
+        theme = "gnzh";
+        plugins = [ "git" ];
+      };
+      promptInit = '' '';
     };
   };
 
@@ -216,8 +242,7 @@ in {
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-label/NIXOS_SD";
+    device = "/dev/disk/by-uuid/${disk}";
     fsType = "ext4";
-    options = [ "noatime" ];
   };
 }
